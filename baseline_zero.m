8b76904c8a8008529f2be14bd181911032314a72
Julian Bouzanquet <julian+github@bouzanquet.com>
2013-02-08 21:46:17 -0800

    Zero version 4, appears functional

--- baseline_zero.m
+++ baseline_zero.m
@@ -1,4 +1,4 @@
-function Aaron_Zero_3 (name)
+function Aaron_Zero_4 (name)
 
 % Grab the data.
 structure = importdata(name);
@@ -18,7 +18,7 @@ signal = structure.data; % The .data part of a structure is the numerical part. 
 min = 100;
 index = 1;
 
-for j = 1:m
+for j = 3:m-3 % I start and end at these points because I want to average nearby points, and I don't want an ArrayOutOfBounds error.
 if abs(signal(j,1) - 1.653) < min % Use absolute value!
     index = j;
     min = abs(signal(j,1) - 1.653); % Use absolute value!
@@ -31,7 +31,7 @@ end
 min2 = 100;
 index2 = 1;
 
-for k = 1:m
+for k = 3:m-3 % I start and end at these points because I want to average nearby points, and I don't want an ArrayOutOfBounds error.
 if abs(signal(k,1) - 1.127) < min2 % Use absolute value!
     index2 = k;
     min2 = abs(signal(k,1) - 1.127); % Use absolute value!
@@ -43,51 +43,54 @@ end
 % our fit by a linear function (called amp) and use that to callibrate our
 % data. 
 
- amplitude1 = signal(index,2) / f(signal(index,1));
- amplitude2 = signal(index2,2) / f(signal(index2,1));
+ average1 = (signal(index-2,2) + signal(index-1,2) + signal(index,2) + signal(index+1,2) + signal(index+2,2))/5.0;
+ amplitude1 = average1 / f(signal(index,1));
+ average2 = (signal(index2-2,2) + signal(index2-1,2) + signal(index2,2) + signal(index2+1,2) + signal(index2+2,2))/5.0;
+ amplitude2 = average2 / f(signal(index2,1));
 
- index
- index2
- 
+
+% This is my way to make the filename of the outputfile 'a_corrected.dat', 
+% if the original file was 'a.something' 
+% Don't judge me!
+
  ar = char(name);
  inder = 1;
  hithere = 0;
  while hithere < 1
  new(inder) = ar(inder);
- inder = inder + 1;
- 
+ inder = inder + 1; 
  if (ar(inder) == '.')
  hithere = 3;
  end
-
  end
  
- new
- inder
  filename = strcat(new,'_corrected.dat');
-fid = fopen(filename, 'w');
+ fid = fopen(filename, 'w');
 
+% Subtract out the callibration and put it in the file.
  
 for i = 1:m
     g(i,1) = signal(i,1);
-    g(i,2) = signal(i,2) - amp(signal(i, 1), amplitude1, amplitude2, signal(index,1), signal(index2,1)) * f(signal(i,1));
+    g(i,2) = signal(i,2) - amp(signal(i, 1)) * f(signal(i,1));
     g(i,3) = signal(i,3);
 
-    fprintf(fid, '%.9f\t%.9f\t%.9f\n', g(i,:));
+    fprintf(fid, '%.9f\t%.9f\t%.9f\n', g(i,:)); % special formatting for the output file
 end
-fclose(fid);
 
-plot(g(:,1),g(:,2))
-    
+fclose(fid); % close the file
+
+% This function is our quadratic fit of the background GaAs wafer.
+
 function y = f(x)
-r = 12400.0/x; 
+r = 12400.0/x; % The original formula is in wavelength, and our data is in eV.
 y = 0.0015873240642803 - 0.00000025667991263308*r +0.000000000011008884166353*r*r;
-end
+end 
 
-function tt = amp(m, y1, y2, x1, x2)
-tt = (y1 - y2) / (x1 - x2) * (m - x2) + y2;
-end
+% This function calculates the final correction to our data. It's a linear
+% fit of our fit, so to speak. This process skews the fit by multiplying a
+% line to the original background.
 
-    
-    
+function correction = amp(x)
+correction = (amplitude2 - amplitude1)/(signal(index2,1) - signal(index,1)) * (x - signal(index, 1)) + amplitude1;
+end
 end
