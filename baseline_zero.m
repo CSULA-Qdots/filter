d61b359e4fb08ec0ec7497d60012e879ef78c320
Julian Bouzanquet <julian+github@bouzanquet.com>
2013-02-08 21:41:16 -0800

    Zero version 2

--- baseline_zero.m
+++ baseline_zero.m
@@ -1,70 +1,74 @@
-function baseline_zero(name)
-
+function Aaron_Zero_2 (name)
 structure = importdata(name);
-global signal;
 signal = structure.data;
 
 [m, n] = size(signal);
 
-slope(1,1) = 1;
-slope(1,2) = 0;
-for i = 2:m
-slope(i,1) = i;
-slope(i,2) = (signal(i,2) - signal(i-1,2))/(signal(i,1) - signal(i-1,1));
-end
+ min = 100;
+ index = 1;
 
-flagger(1,1) = 1;
-flagger(1,2) = 1;
-for j = 2:m-1
-flagger(j,1) = j;
-flagger(j,2) = 0;
-
-if slope(j,2) * slope(j+1,2) < 0
-flagger(j,2) = 1;
+for j = 1:m
+if abs(signal(j,1) - 1.653) < min
+    index = j;
+    min = abs(signal(j,1) - 1.653);
 end
 end
-flagger(m,1) = m;
-flagger(m,2) = 1;
 
-min = signal(1,2);
-index = 1;
+ min2 = 100;
+ index2 = 1;
 
 for k = 1:m
-trial = signal(k,2);
-if (trial < min && flagger(k,2) == 1)
-    index = k;
-    min = signal(k,2);
-end
-end
-
-flagger(index, 2) = 0;
-
-index
-min
-
-index2 = index;
-
-while abs(index2 - index) < 20
-flagger(index2,2) = 0;
-min2 = 1000;
-index2 = 10;
-
-for k = 1:m
-trial = signal(k,2);
-if (trial < min2 && flagger(k,2) == 1)
+if abs(signal(k,1) - 1.127) < min2
     index2 = k;
-    min2 = signal(k,2);
+    min2 = abs(signal(k,1) - 1.127);
 end
 end
-end
-index2
-min2
 
+ amplitude1 = signal(index,2) / f(signal(index,1))
+ amplitude2 = signal(index2,2) / f(signal(index2,1))
+
+ index
+ index2
+ 
+ ar = char(name);
+ inder = 1;
+ hithere = 0;
+ while hithere < 1
+ new(inder) = ar(inder);
+ inder = inder + 1;
+ 
+ if (ar(inder) == '.')
+ hithere = 3;
+ end
+
+ end
+ 
+ new
+ inder
+ filename = strcat(new,'_corrected.dat');
+fid = fopen(filename, 'w');
+
+ 
 for i = 1:m
-signal(i,2) = signal(i,2) - min + (-min2 + min)/(signal(index2,1) - signal(index,1))*(signal(i,1) - signal(index,1)); 
+    g(i,1) = signal(i,1);
+    g(i,2) = signal(i,2) - amp(signal(i, 1), amplitude1, amplitude2, signal(index,1), signal(index2,1)) * f(signal(i,1));
+    g(i,3) = signal(i,3);
+
+    fprintf(fid, '%.9f\t%.9f\t%.9f\n', g(i,:));
+end
+fclose(fid);
+
+plot(g(:,1),g(:,2))
+    
+    
+    
 end
 
-signal
+function y = f(x)
+r = 12400.0/x; 
+y = 0.0015873240642803 - 0.00000025667991263308*r +0.000000000011008884166353*r*r;
+end
 
-plot(signal(:,1), signal(:,2))
+function tt = amp(m, y1, y2, x1, x2)
+tt = (y1 - y2) / (x1 - x2) * (m - x2) + y2;
 end
