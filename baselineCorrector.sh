19c7f2c22507e163a30db7dc71f3a53d18c07e9f
Julian Bouzanquet <julian+github@bouzanquet.com>
2013-03-01 14:01:16 -0800

    Cleanup and rename

--- baselineCorrector.sh
+++ baselineCorrector.sh
@@ -1,4 +1,4 @@
-#Code taken from discussion at http://stackoverflow.com/questions/2001183/how-to-call-matlab-functions-from-the-linux-command-line
+#Code modified from http://stackoverflow.com/questions/2001183/how-to-call-matlab-functions-from-the-linux-command-line
 list_Of_Names=(*_eV.out.dat)
 tmpMatFile=$(mktemp)
 matlabExecutable=matlab
@@ -6,9 +6,7 @@ matFunction=zero
 for i in ${list_Of_Names[@]}
 do
 	echo "Running function" $matFunction " on file " $i
-	string=$i
 	echo "${matFunction}('${i}')" > tmpMatFile
-	#cat tmpMatFile
 	${matlabExecutable} -nojvm -nodisplay -nosplash < tmpMatFile
 done
 rm tmpMatFile
