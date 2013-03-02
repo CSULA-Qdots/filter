eea4580fa915af36013edde5291f4a35dad5f54a
Julian Bouzanquet <julian+github@bouzanquet.com>
2013-03-02 13:39:27 -0800

    Added logging to baselineCorrector

--- baselineCorrector.sh
+++ baselineCorrector.sh
@@ -1,4 +1,5 @@
 #Code modified from http://stackoverflow.com/questions/2001183/how-to-call-matlab-functions-from-the-linux-command-line
+unset DISPLAY
 list_Of_Names=(*_eV.out.dat)
 tmpMatFile=$(mktemp)
 matlabExecutable=matlab
@@ -7,6 +8,6 @@ for i in ${list_Of_Names[@]}
 do
 	echo "Running function" $matFunction " on file " $i
 	echo "${matFunction}('${i}')" > tmpMatFile
-	${matlabExecutable} -nojvm -nodisplay -nosplash < tmpMatFile
+	${matlabExecutable} -nojvm -nodisplay -nosplash &>> log-baseline-$(date +%Y%m%d%H%M%S) < tmpMatFile
 done
 rm tmpMatFile
