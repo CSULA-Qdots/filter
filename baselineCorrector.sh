63c9f96e80a557c3c28b0dbe76024b3e9ad421fe
Julian Bouzanquet <julian+github@bouzanquet.com>
2013-03-07 17:32:27 -0800

    Fix accidental commit reversion, report version in log.

--- baselineCorrector.sh
+++ baselineCorrector.sh
@@ -1,13 +1,17 @@
-#Code modified from http://stackoverflow.com/questions/2001183/how-to-call-matlab-functions-from-the-linux-command-line
+#/bin/bash
 unset DISPLAY
 list_Of_Names=(*_eV.out.dat)
-tmpMatFile=$(mktemp)
+logFile=log-baseline-$(date +%Y%m%d%H%M%S)
 matlabExecutable=matlab
 matFunction=zero
+tmpMatFile=$(mktemp)
+version=1.1
+echo "Baseline correction version $version" > $logFile
+echo $(head -n1 zero.m) >> $logFile
 for i in ${list_Of_Names[@]}
 do
 	echo "Running function" $matFunction " on file " $i
-	echo "${matFunction}('${i}')" > tmpMatFile
-	${matlabExecutable} -nojvm -nodisplay -nosplash &>> log-baseline-$(date +%Y%m%d%H%M%S) < tmpMatFile
+	echo "${matFunction}('${i}')" > $tmpMatFile
+	${matlabExecutable} -nojvm -nodisplay -nosplash &>> $logFile < $tmpMatFile
 done
-rm tmpMatFile
+rm $tmpMatFile
