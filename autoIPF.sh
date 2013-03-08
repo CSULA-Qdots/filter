7093a0ead93f064ac154e323bc223c3975a47883
Julian Bouzanquet <julian+github@bouzanquet.com>
2013-03-07 16:48:31 -0800

    Partial work, not functional yet.

--- autoIPF.sh
+++ autoIPF.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-unset DISPLAY
 list_Of_Names=(*.out.dat)
 #Logging doesn't seem necessary when creating a spreadsheet.
 #logFile=log-ipf-$(date +%Y%m%d%H%M%S)
@@ -9,8 +8,14 @@ tmpMatFile=$(mktemp)
 for i in ${list_Of_Names[@]}
 do
 	echo "Running function" $matFunction " on file " $i
-	echo "${matFunction}('${i}')" > $tmpMatFile
+#	As in zero.m
+	echo "dataStructure = importdata(char('$i'));" > $tmpMatFile
+	echo "${matFunction}(dataStructure.data)" >> $tmpMatFile
+#	echo "${matFunction}(dataStructure.data(:,1),dataStructure.data(:,3))" >> $tmpMatFile
+	echo pause >> $tmpMatFile
 #	${matlabExecutable} -nojvm -nodisplay -nosplash &>> $logFile < $tmpMatFile
-	${matlabExecutable} -nojvm -nodisplay -nosplash < $tmpMatFile
+#	Wait option prevents running the loop concurrently, as "matlab" is actually a launcher script
+	${matlabExecutable} -nosplash < $tmpMatFile
 done
+cat $tmpMatFile
 rm $tmpMatFile
