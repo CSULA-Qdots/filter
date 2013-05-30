775eb07df20a27f700c2f708a4596ee270a140a3
Julian Bouzanquet <julian+github@bouzanquet.com>
2013-05-29 17:22:31 -0700

    Implemented conditionals to stop on error

--- mcp.sh
+++ mcp.sh
@@ -4,47 +4,52 @@
 
 #Remove NewStuff.dat file, containing last run's fits
 #sleep waits 5 seconds for user to realize potential mistake
-if (test -e NewStuff.dat); then
+if [ -e NewStuff.dat ]; then
   echo Removing NewStuff.dat ... ctrl-C to cancel
   sleep 5
   rm NewStuff.dat
-fi
+fi #/NewStuff
 
 echo Filtering data
-tclsh filter.tcl $@
+#Only continue if Filter prceeded error-free
+if (tclsh filter.tcl $@); then
 
-#Operate on files output by filter
-#sed (S)earches for ".dat", replacing with ".out.dat", (G)lobally
-outfiles=$(echo ${@} | sed -e "s/.dat/.out.dat/g")
+  #Operate on files output by filter
+  #sed (s)earches for ".dat", replacing with ".out.dat", (g)lobally
+  outfiles=$(echo ${@} | sed -e "s/.dat/.out.dat/g")
 
-#Return code reading example
-#if (./sort.sh $outfiles != 0 ); then
-#  echo Sort failed
-#  exit 1
-#fi
+  echo Subtracting GaAs baseline
+  #Only continue if Baseline prceeded error-free
+  if (bash baseline.sh $outfiles); then
 
-echo Subtracting GaAs baseline
-./baseline.sh $outfiles
+    #Operate on files output by baseline
+    correctedfiles=$(echo ${@} | sed -e "s/.dat/_corrected.dat/g")
 
-#Operate on files output by baseline
-correctedfiles=$(echo ${@} | sed -e "s/.dat/_corrected.dat/g")
+    echo Calling IPF
+    #No return code possible because Matlab launcher closes before Matlab does
+    bash multiIPF.sh $correctedfiles
 
-echo Calling IPF
-./multiIPF.sh $correctedfiles
+    #Move out files from current directory to prevent tripling the already large number present.
+    echo Cleanup
+    #p option prevents error if folder is extant
+    mkdir -p out
+    mkdir -p corrected
+    mv $outfiles out/
+    mv $correctedfiles corrected/
 
-#Move out files from current directory to prevent tripling the already large number present.
-echo Cleanup
-#p option prevents error if folder is extant
-mkdir -p out
-mkdir -p corrected
-mv $outfiles out/
-mv $correctedfiles corrected/
-
-#Rename generic output file to current date
-#Date code used is YYYYMMDDHMS, such as 20130115161205, matching logs from Filter
-if (test -e NewStuff.dat); then
-    mv NewStuff.dat fits-$(date +%Y%m%d%H%M%S).txt
+    #Rename generic output file to current date
+    #Date code used is YYYYMMDDHMS, such as 20130115161205, matching logs from Filter
+    if [ -e NewStuff.dat ]; then
+	mv NewStuff.dat fits-$(date +%Y%m%d%H%M%S).txt
+    else
+	echo Something went wrong, no fits found.
+	exit 1 #Script failed, return non-zero code
+    fi #/NewStuff
   else
-    echo Something went wrong, no fits found.
-    exit 1 #Script failed, return non-zero code
-fi
+    echo Zeroing failed
+    exit 1
+  fi #/Baseline
+else
+    echo Filtering failed
+    exit 1
+fi #/Filter
