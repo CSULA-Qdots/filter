66c2ac822f1e25cae049a82a45c65427eeb70f2d
Julian Bouzanquet <julian+github@bouzanquet.com>
2013-06-01 16:51:07 -0700

    Added laser detection to MCP, with waiting period before file clean-up

--- mcp.sh
+++ mcp.sh
@@ -10,17 +10,33 @@ if [ -e NewStuff.dat ]; then
   rm NewStuff.dat
 fi #/NewStuff removal
 
+#Operate separately on each laser color, due to laser second order
+#Translate literal space characters of input to newline character,
+#	then search for laser string
+redfiles=$(echo $@ | tr ' ' '\n' | grep RMG | xargs)
+greenfiles=$(echo $@ | tr ' ' '\n' | grep 20G | xargs)
+#ogreenfiles=$(echo $@ | tr ' ' '\n' | grep 5G | xargs)
+#violetfiles=$(echo $@ | tr ' ' '\n' | grep 5V | xargs)
+#oredfiles=$(echo $@ | tr ' ' '\n' | grep 5R | xargs)
+
 echo "Filtering data"
 #Only continue if Filter prceeded error-free
-if (tclsh filter.tcl --sortby=ev -- $@); then
+if (tclsh filter.tcl --sortby=ev -- $redfiles); then
+if (tclsh filter.tcl --sortby=ev -- $greenfiles); then
 
   #Operate on files output by filter
   #sed (s)earches for ".dat", replacing with ".out.dat", (g)lobally
+  
   outfiles=$(ls $@ | cat | xargs | sed -e "s/.dat/.out.dat/g")
+  outgreenfiles=$(ls $greenfiles | cat | xargs | sed -e "s/.dat/.out.dat/g")
+  outredfiles=$(ls $redfiles | cat | xargs | sed -e "s/.dat/.out.dat/g")
+  
+  rejectfiles=$(ls $@ | cat | xargs | sed -e "s/.dat/.out.dat.reject/g")
 
   echo "Subtracting GaAs baseline"
   #Only continue if Baseline prceeded error-free
-  if (bash baseline.sh $outfiles); then
+  if (bash baseline.sh $outredfiles); then
+  if (bash baseline.sh $outgreenfiles); then
 
     #Operate on files output by baseline
     correctedfiles=$(ls $@ | cat | xargs | sed -e "s/.dat/_corrected.dat/g")
@@ -30,12 +46,16 @@ if (tclsh filter.tcl --sortby=ev -- $@); then
     bash multiIPF.sh $correctedfiles
 
     #Move out files from current directory to prevent tripling the already large number present.
-    echo "Cleanup"
+    echo "Press enter to clean up"
+    #Waits for input. We ignore it
+    read
     #p option prevents error if folder is extant
-    mkdir -p out
     mkdir -p corrected
-    mv $outfiles out/
+    mkdir -p out
+    mkdir -p reject
     mv $correctedfiles corrected/
+    mv $outfiles out/
+    mv $rejectfiles reject/
 
     #Rename generic output file to current date
     #Date code used is YYYYMMDDHMS, such as 20130115161205, matching logs from Filter
@@ -48,8 +68,10 @@ if (tclsh filter.tcl --sortby=ev -- $@); then
   else
     echo "Zeroing failed"
     exit 1
-  fi #/Baseline conditional
+  fi #/Baseline green conditional
+  fi #/Baseline red conditional
 else
     echo "Filtering failed"
     exit 1
-fi #/Filter conditional
+fi #/Filter green conditional
+fi #/Filter red conditional
